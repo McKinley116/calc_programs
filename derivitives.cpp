c24b36dbf0cbeee93f8e3c31a6acb9fadcc2b3a6
brandonmckinley <brandon.s.mckinley@gmail.com>
2025-10-14 13:10:46 -0700

    building boilerplate

---
+++ derivitives.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Brandon McKinley on 10/13/25.
+//
+//Power Rule : d/dx x^a = a*x^a-1
+//Chain rule : d/dx x(ax) = a x(ax)
+//Constant : d/dx C = C
+// d/dx e^x = e^x
+// d/dx sin x = cos x
+// d/dx cos x = -sin x
+// d/dx tan x = sec^2 x
+// d/dx sec x = secx tanx
+// d/dx csc x = -cscx cotx
+// d/dx cot x = -csc x
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+//Functions
+void powerRule();
+void chainRule();
+void constantRule();
+void determineInput();
+
+int main ()
+
+{
+    double userChoice;
+    double answer;
+    string trig[10] {};
+    double constant[10] {0};
+
+}
