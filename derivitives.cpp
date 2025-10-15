f6d6e3e81a076b1f716255058150a3dcf11969f6
brandonmckinley <brandon.s.mckinley@gmail.com>
2025-10-15 15:25:53 -0700

    starting over, building a basic calculator

--- derivitives.cpp
+++ derivitives.cpp
@@ -21,28 +21,70 @@
 
 using namespace std;
 
-//Functions
-int addition(int a, int b) {
-    return a + b;
-};
-int subtraction(int a, int b) {
-    return a - b;
-};
-int multiplication(int a, int b) {
-    return a * b;
-};
-int division(int a, int b) {
-    return a / b;
-};
-string trig_derive();
-int const_derive();
-string powerRule();
+// Function declarations
+void determineOperation(int a, int b, char operation);
+int addition(int a, int b);
+int subtraction(int a, int b);
+int multiplication(int a, int b);
+int division(int a, int b);
+string trig_derive(string a);
+int const_derive(int a);
+string powerRule(int a, int x);
 string productRule();
 string quotientRule();
 
-int main ()
-
-{
+int main() {
     int a, b;
+    char operation;
 
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Enter an operation (+, -, *, /, d/dx): ";
+    cin >> operation;
+
+    determineOperation(a, b, operation);
+
+    return 0;
 }
+
+void determineOperation(int a, int b, char operation) {
+    if (operation == '+') {
+        cout << "Result: " << addition(a, b) << endl;
+    } else if (operation == '-') {
+        cout << "Result: " << subtraction(a, b) << endl;
+    } else if (operation == '*') {
+        cout << "Result: " << multiplication(a, b) << endl;
+    } else if (operation == '/') {
+        if (b != 0) {
+            cout << "Result: " << division(a, b) << endl;
+        } else {
+            cout << "Error: Division by zero is not allowed." << endl;
+        }
+
+    }else if (operation == 'd/dx') {
+
+    }else {
+        cout << "Invalid operation." << endl;
+    }
+}
+
+int addition(int a, int b) {
+    return a + b;
+}
+
+int subtraction(int a, int b) {
+    return a - b;
+}
+
+int multiplication(int a, int b) {
+    return a * b;
+}
+
+int division(int a, int b) {
+    return a / b;
+}
+
+string trig_derive(string trig) {
+
+
+};
