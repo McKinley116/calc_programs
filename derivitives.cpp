c7e20100dc679295a03f66dd72b8be9b7df0f3bf
brandonmckinley <brandon.s.mckinley@gmail.com>
2025-10-20 14:57:11 -0700

    working on determineOperation function. output is Enter an equation:2+3                                                   This is your equation:                                                   2+3error                                                   error

--- derivitives.cpp
+++ derivitives.cpp
@@ -22,17 +22,10 @@
 using namespace std;
 
 // Function declarations
-//void breakdown(string equation, vector<string> words);
+void breakdown(string equation, vector<string> words, vector<int> constants); // this function breaks down a string equation into seperate parts to determine what operations to use
 int determineOperation( vector<string> words, string equation);
-int addition(int a, int b);
-int subtraction(int a, int b);
-int multiplication(int a, int b);
-int division(int a, int b);
-string trig_derive( vector<string>, vector<string> );
-int const_derive(int a);
-string powerRule(int a, int x);
-string productRule();
-string quotientRule();
+int determineConstants(vector<string> words, string equation, vector<int> constants);
+
 
 // Classes
 
@@ -42,7 +35,7 @@ int main() {
     string equation;
     string word;
     vector<string> words;
-    int answer;
+    vector<int> constants;
 
     cout << "Enter an equation:";
     cin.clear();
@@ -58,31 +51,39 @@ int main() {
    cout << "This is your equation:" << endl;
 
     for (int i = 0; i < words.size(); i++) {
-        cout << words[i];
+        cout << words[i] << " " ;
     }
-
-    determineOperation(words, equation);
-    answer = determineOperation(words, equation);
-    cout << "The answer is:" << answer << endl;
-
-
 
     return 0;
 }
 
-int determineOperation( vector<string> words, string equation) {
+void breakdown(string equation, vector<string> words, vector<int> constants) {
+    //First determine where each char or integer goes
+    //second sends them into their respective vectors
+}
+
+int determineConstants(string equation, int a, int b) {
+    // this function determines what constats are in an equation and the stores them into its own vector for computation
+
+    for (int i = 0; i < equation.size(); i++) {
+
+    }
+}
+
+
+int determineOperation( vector<string> words, string equation, int a, int b) {
     // this functions loops through a users equation and determines the operation to use to calculate an answer
     int answer = 0;
         for (int i = 0; i < words.size(); i++) {
             for ( int j = 0; j < words[i].size(); j++) {
                 if (words[i][j] == '+') {
-                    answer = addition();
+                    //answer = addition(a,b);
                 } else if (words[i][j] == '-') {
-                    answer = subtraction();
+                    //answer = subtraction(a, b);
                 } else if (words[i][j] == '*') {
-                    answer = multiplication();
+                    //answer = multiplication(a,b);
                 } else if (words[i][j] == '/') {
-                    answer = division();
+                    //answer = division(a,b);
                 } else (cout << "error" << endl);
             }
         }
@@ -90,6 +91,20 @@ int determineOperation( vector<string> words, string equation) {
     return answer;
 
     }
+
+
+
+/*
+int addition(int a, int b);
+int subtraction(int a, int b);
+int multiplication(int a, int b);
+int division(int a, int b);
+string trig_derive( vector<string>, vector<string> );
+int const_derive(int a);
+string powerRule(int a, int x);
+string productRule();
+string quotientRule();
+
 
 int addition(int a, int b) {
     return a + b;
@@ -106,4 +121,4 @@ int multiplication(int a, int b) {
 int division(int a, int b) {
     return a / b;
 }
-
+ */
